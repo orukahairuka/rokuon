50578b90109150bc0fb0e92236fb4b9c1e638bea
irukahaoruka <erika03301109@gmail.com>
2025-03-03 11:41:07 +0900

    テストで作ったものを追加

---
+++ rokuon/rokuonApp.swift
@@ -0,0 +1,17 @@
+//
+//  rokuonApp.swift
+//  rokuon
+//
+//  Created by 櫻井絵理香 on 2025/02/04.
+//
+
+import SwiftUI
+
+@main
+struct rokuonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
